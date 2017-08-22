98117d3ea485920f463190f2a0cc476f6d123d68
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2017-08-22 15:02:08 +0530

    leap1.c

---
+++ leap1.c
@@ -0,0 +1,17 @@
+#incllude<stdio.h>
+#include<conio.h>
+void main()
+{
+int year;
+prinft("enter the year");
+scanf("%d",&year);
+if(year%4==0)
+{
+printf("the year is not leap year");
+}
+else
+{
+printf("the year is leap year");
+}
+getch();
+}
