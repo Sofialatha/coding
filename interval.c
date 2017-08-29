f772dd566d696140ac538201a6411ef617291ddc
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2017-08-29 16:05:01 +0530

    interval.c

---
+++ interval.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,m=10,n=20;
+printf("the odd numbers are",\n);
+for(num=m;num<=n;num++)
+{
+if(n%2==1)
+{
+printf("%d",num);
+}
+getch();
+}
