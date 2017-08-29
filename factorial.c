64f85594392c11166cb7719a0c9666db67937f3a
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2017-08-29 16:35:50 +0530

    factorial.c

---
+++ factorial.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int fact=1,i,n;
+printf("value of n");
+scanf("%d",&n);
+for(i=0;i<=n;i++);
+{
+fact=fact*i;
+}
+printf("the factorial of number is",%d);
+getch();
+}
