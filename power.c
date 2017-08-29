2bff566135a56d6f8018724a09e4a999ca06149d
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2017-08-29 15:38:51 +0530

    power.c

---
+++ power.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#inlude<conio.h>
+void main()
+{
+long long=1;
+int base,exponent;
+printf("enter the base");
+scanf("%d",&base);
+printf("enter the exponent");
+scanf("%d",&exponent);
+while(exponent!=0)
+{
+result *=1;
+--exponent;
+printf("the answer is:"%d);
+getch();
+}
