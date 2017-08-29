a18c5aa668f52115e33e76fcede926daf3d46879
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2017-08-29 15:49:03 +0530

    palindrom.c

---
+++ palindrom.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,reversed integer=0,remainder,original integer;
+printf("enter an integer");
+scanf("%d",&n);
+originalinteger=n;
+while(n!=0)
+remainder=n%10;
+reversedinteger=reversedunteger*10+remainder;
+n/=10;
+if(originalinteger==reversedinteger)
+{
+printf("the number is palindrom");
+}
+getch();
+}
