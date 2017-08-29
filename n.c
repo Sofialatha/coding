d9b8698fba7191bbdc89267e24d25b79f128c3e1
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2017-08-29 14:53:55 +0530

    n.c

---
+++ n.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,sum=0;
+printf("enter the number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("%d",i);
+sum=sum+i;
+}
+printf("sum of %d is"%d,n,sum);
+getch();
+}
