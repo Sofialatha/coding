8b756807f79dbf1ce8a0e0106233b5299fa46087
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-01-08 05:59:21 -0500

    power.java

---
+++ power.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class power
+{
+public static voidmain(string[] args)
+{
+iny base=2;exponent=3;
+long result=0;
+while(exponent!=0)
+{
+result*=base;
+--exponent;
+}
+}
+}
