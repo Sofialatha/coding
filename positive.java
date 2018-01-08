3dd62fdd7580e5980bb79af9b9915e40f809b8a8
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-01-08 05:41:09 -0500

    positive.java
    positive.java

---
+++ positive.java
@@ -0,0 +1,29 @@
+import java.io,*;
+import java.util.*;
+class checking
+{
+public static voidmain(string args[])
+{
+iny num;
+scanner sc=new scanner(system.in)
+system.out.println("enter the number"(;
+{
+for(i=1;i<=100000;i++)
+{
+num=sc.nextint();
+}
+}
+if(n<0)
+{
+system.out.println("the number is negative")
+}
+else if(n>0)
+{
+system.out.println("the number is positive");
+}
+else
+{
+system.out.println("the number is zero")
+}
+}
+}
