9b58828e8150924bfa90ed2bf1b4d734fd4fc921
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-01-17 05:47:08 -0500

    maxarray.java

---
+++ maxarray.java
@@ -0,0 +1,17 @@
+import java.util.scanner*;
+int n,max;
+scanner s=new scanner(system.in);
+system.out.println("enter the number of array");
+n=s.nextint();
+system.out.println("enter the elements");
+for(i=0;i<n;i++);
+{
+a[i]=s.nextint();
+}
+max=a[0];
+for(i=0;i<n;i++)
+{
+if(max<a[i])
+{
+max=a[i];
+}
