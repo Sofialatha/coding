b44ebc8ad44b32da95327acb5c415f8b0f9a3125
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-01-17 06:08:24 -0500

    numeric.java

---
+++ numeric.java
@@ -0,0 +1,24 @@
+impotr java.io.*;
+public class numeric
+{
+public static voidmain(string[]args)
+{
+string string="2";
+boolean numeric=true;
+try{
+bouble num=double.parsedouble(string);
+}
+catch{
+numberformate exception e)
+{
+numeric=false;
+}
+if(mumeric)
+system.out.println("the string is a number");
+}
+else
+{
+system.out.println("the string is not a number");
+}
+}
+}
